4c3232dcf1b22d5d7998410119d5494944f29eff
maxim <makr781@ess.ais.ac.nz>
2022-02-27 16:08:44 +1300

    initial

---
+++ UseUserInputClass.cs
@@ -0,0 +1,11 @@
+﻿namespace DevelopUserInput.UseUserInput
+{
+    public class UseUserInputClass
+    {
+        public bool UseUserInput(string name)
+        {
+            if (name == "max") return true;
+            return false;
+        }
+    }
+}
